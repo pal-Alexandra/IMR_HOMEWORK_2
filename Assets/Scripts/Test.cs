3fee26be55b931a37785a1f15106e19df5f50147
Pal Alexandra <palalexandra3@yahoo.com>
2023-10-16 16:22:55 +0300

    dummy score implemented

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public ScoreScript score;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            score.AddScore(1);
+        }
+        
+    }
+}
